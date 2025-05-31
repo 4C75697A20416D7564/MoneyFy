0931225a44d09c92f57fa09ecdf4da3e9dfbef0b
Luiz Felipe Moralez Amud <amud@MacBook-Pro-de-Luiz.local>
2025-05-31 00:20:49 -0300

    Config swagger e iniciando documentacao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 import { routes } from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument));
 
 app.use(express.json());
 
